cf270e51633248864d2165d8ccb948bd9a7e1fdd
felipe <feot1000@gmail.com>
2026-03-13 09:25:25 +0100

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './styles/lunar-variables.css'
 import './styles/lunar-base.css'
 import './styles/lunar-components.css'
 import './styles/lunar-animations.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>,
+)
